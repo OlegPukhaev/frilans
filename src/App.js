4f0c293e3b02bd8500fa6dcbd3c33cb86b66163f
Oleg Pukhaev <nuba8184@gmail.com>
2018-08-23 14:27:39 +0600

    Connect to server 200

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// ../../node_modules/j-toker/src/
+var Auth = require('./j-toker.js');
+    
+Auth.configure({
+    apiUrl: '../api'
+});
+
 class App extends Component {
+  constructor (){
+    super();
+
+    this.state = {
+                    email: "nuba8184@gmail.com",
+                    password: "12345678",
+                    apiUrl: '../api'
+                };
+  }
+
+ handleRegistrationClick() {
+
+      Auth.emailSignUp({
+        email: 'test@test.com',
+        password: '123456',
+        password_confirmation: '123456',
+        config: 'altUser'
+      })
+      .then(response => {
+            console.log("yes");
+          })
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +43,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          {this.handleRegistrationClick()}
         </p>
       </div>
     );
